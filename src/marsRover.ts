0644aa370ccccc6b3c61317416b0239e44e20a9b
MatteoPierro <pierromatteo@gmail.com>
2019-04-09 16:34:53 +0200

    extract left and right directions in private methods

--- src/marsRover.ts
+++ src/marsRover.ts
@@ -12,20 +12,30 @@ export class MarsRover {
     }
 
     executeCommands(commands: Command[]) {
+        this.direction = commands[0] === Command.TurnLeft
+            ? this.leftDirections().get(this.direction)
+            : this.rightDirections().get(this.direction);
+    }
+
+    private leftDirections(): Map<Direction, Direction> {
         const leftDirections = new Map<Direction, Direction>();
+        
         leftDirections.set(Direction.North, Direction.West);
         leftDirections.set(Direction.South, Direction.East);
         leftDirections.set(Direction.East, Direction.North);
         leftDirections.set(Direction.West, Direction.South);
 
+        return leftDirections;
+    }
+
+    private rightDirections(): Map<Direction, Direction> {
         const rightDirections = new Map<Direction, Direction>();
+        
         rightDirections.set(Direction.North, Direction.East);
         rightDirections.set(Direction.South, Direction.West);
         rightDirections.set(Direction.East, Direction.South);
         rightDirections.set(Direction.West, Direction.North);
         
-        this.direction = commands[0] === Command.TurnLeft
-            ? leftDirections.get(this.direction)
-            : rightDirections.get(this.direction);
+        return rightDirections;
     }
 }
