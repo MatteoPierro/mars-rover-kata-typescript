11491386eea427aef1984135b91f2fe2362e0ac0
MatteoPierro <pierromatteo@gmail.com>
2019-04-09 15:54:51 +0200

    Use of startPosition as Position type

--- src/marsRover.ts
+++ src/marsRover.ts
@@ -1,9 +1,12 @@
 import { Position } from "./position";
+import { Direction } from "./direction";
 
 export class MarsRover {
     position: Position;
+    direction: Direction;
 
-    constructor(startPosition: Position) {
-        this.position = startPosition;    
+    constructor(startPosition: Position, startDirection: Direction) {
+        this.position = startPosition;  
+        this.direction = startDirection;  
     }
 }
--- test/marsRover.spec.ts
+++ test/marsRover.spec.ts
@@ -1,12 +1,15 @@
 import { MarsRover } from "../src/marsRover";
 import { Position } from "../src/position";
+import { Direction } from "../src/direction";
 
 describe('Mars Rover', () => {
     it('should start from a given initial point', () => {
         const startPosition = new Position(0, 0);
+        const startDirection = Direction.North;
 
-        const rover = new MarsRover(startPosition);
+        const rover = new MarsRover(startPosition, startDirection);
         
         expect(rover.position).toBe(startPosition);
+        expect(rover.direction).toBe(startDirection);
     });
 });
