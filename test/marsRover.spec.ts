a6ca84b987a526f3becc65f13ff57192d1d42b32
MatteoPierro <pierromatteo@gmail.com>
2019-04-09 15:48:00 +0200

    check using position object

--- test/marsRover.spec.ts
+++ test/marsRover.spec.ts
@@ -1,10 +1,10 @@
 import { MarsRover } from "../src/marsRover";
+import { Position } from "../src/position";
 
 describe('Mars Rover', () => {
     it('should start from a given initial point', () => {
         const rover = new MarsRover(0,0);
         
-        expect(rover.position.x).toBe(0);
-        expect(rover.position.y).toBe(0);
+        expect(rover.position).toEqual(new Position(0,0));
     });
 });
