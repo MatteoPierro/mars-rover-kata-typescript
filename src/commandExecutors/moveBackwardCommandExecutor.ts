cf02bc12715ce629ff5c84791344f85052932183
MatteoPierro <pierromatteo@gmail.com>
2019-04-17 12:45:43 +0200

    implements move backward command executor

--- src/commandExecutors/moveBackwardCommandExecutor.ts
+++ src/commandExecutors/moveBackwardCommandExecutor.ts
@@ -1,13 +1,26 @@
-import { CommandExecutor } from './commandExecutor';
-import { RoverState } from "../roverState";
-import { Position } from "../position";
+import { MoveCommandExecutor } from './moveCommandExecutor';
+import { Direction } from '../direction';
 
-export class MoveBackwardCommandExecutor implements CommandExecutor {
-    execute(currentState: RoverState): RoverState {
-        let newX = currentState.position.x;
-        let newY = currentState.position.y - 1;
-        let newPosition = new Position(newX, newY);
-        let newDirection = currentState.direction;
-        return new RoverState(newPosition, newDirection);
+export class MoveBackwardCommandExecutor extends MoveCommandExecutor {
+    protected xSteps(): Map<Direction, number> {
+        const steps = new Map<Direction, number>();
+
+        steps.set(Direction.North, 0);
+        steps.set(Direction.East, -1);
+        steps.set(Direction.West, 1);
+        steps.set(Direction.South, 0);
+
+        return steps;
+    }
+
+    protected ySteps(): Map<Direction, number> {
+        const steps = new Map<Direction, number>();
+
+        steps.set(Direction.North, -1);
+        steps.set(Direction.East, 0);
+        steps.set(Direction.West, 0);
+        steps.set(Direction.South, 1);
+
+        return steps;
     }
 }
--- test/commandExecutors/moveBackwardCommandExecutor.spec.ts
+++ test/commandExecutors/moveBackwardCommandExecutor.spec.ts
@@ -14,11 +14,27 @@ describe('MoveBackwardCommandExecutor', () => {
         expect(newState.position).toEqual(new Position(0, 0));
     });
 
-    xit('should move forward along y when is facing south and receive backward command', () => {
+    it('should move forward along y when is facing south and receive backward command', () => {
         const currentState = new RoverState(new Position(0, 0), Direction.South);
 
         const newState = executor.execute(currentState);
 
         expect(newState.position).toEqual(new Position(0, 1));
+    });
+
+    it('should move backward along x when is facing east and receive backward command', () => {
+        const currentState = new RoverState(new Position(1, 0), Direction.East);
+
+        const newState = executor.execute(currentState);
+
+        expect(newState.position).toEqual(new Position(0, 0));
+    });
+
+    it('should move forward along x when is facing west and receive backward command', () => {
+        const currentState = new RoverState(new Position(0, 0), Direction.West);
+
+        const newState = executor.execute(currentState);
+
+        expect(newState.position).toEqual(new Position(1, 0));
     });
 });
